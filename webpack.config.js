3e64ee8bc9e913107d86593b8244718d1f1a5b93
skim6281 <skim6281@gmail.com>
2017-03-01 17:51:36 -0500

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: "./lib/silly_rabbit.js",
+  output: {
+    filename: "./bundle.js"
+  },
+  module: {
+   loaders: [
+     {
+       test: [/\.jsx?$/],
+       exclude: /(node_modules)/,
+       loader: 'babel-loader',
+       query: {
+         presets: ['es2015']
+       }
+     }
+   ]
+ },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js"]
+  }
+};
